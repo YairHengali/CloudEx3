6a989aeb6ca99b02e6dbb32a029009eab8132cc2
ofirNakdai <93441082+ofirNakdai@users.noreply.github.com>
2023-07-14 16:30:30 +0300

    Update assn3_tests.py

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -73,4 +73,4 @@ def test_7_get_meals(base_url):
 def test_8_post_meal_with_same_name(base_url):
         response = requests.post(f"{base_url}/meals", json={"name": "delicious", "appetizer" : 1, "main" : 1, "dessert" : 1})
         assert response.status_code in [400, 422] #the return status code from the request is 400 or 422
-        assert int(response.text) == -1 #the code is -2 (same meal name as existing meal) 
+        assert int(response.text) == -2 #the code is -2 (same meal name as existing meal) 
