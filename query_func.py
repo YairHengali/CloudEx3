cdd454ed44c40b4d4b280a11fd93dfa794628078
Yair Hengali <yairhengali@gmail.com>
2023-07-14 17:11:54 +0300

    added query job

--- query_func.py
+++ query_func.py
@@ -1,6 +1,12 @@
 import requests
 
-def query_resolver(fileName:String):
+def query_resolver():
   with open("query.txt", "r") as file:
-    for line in file:
-      response = requests.get(url, headers=headers, params=querystring)
+    for dish_name in file:
+      post_response = requests.post(f"http://localhost:8000/dishes", json={"name": dish_name})
+      get_response = requests.get(f"http://localhost:8000/dishes/{dish_name}")
+      calories = get_response.json().get("cal")
+      sodium = get_response.json().get("sodium")
+      sugar = get_response.json().get("sugar")
+      print(f"{dish_name} contains {calories} calories, {sodium} mgs of sodium, and {sugar} grams of sugar")
+
