65aba2e27a6db87f0e99283aadf14f4c79f47d36
Yair Hengali <yairhengali@gmail.com>
2023-07-15 14:31:34 +0300

    Added "ERROR" if item causes an error

--- query_func.py
+++ query_func.py
@@ -5,9 +5,12 @@ def query_resolver():
     for dish_name in file:
       dish_name = dish_name.strip()
       post_response = requests.post(f"http://localhost:8000/dishes", json={"name": dish_name})
-      get_response = requests.get(f"http://localhost:8000/dishes/{dish_name}")
-      calories = get_response.json().get("cal")
-      sodium = get_response.json().get("sodium")
-      sugar = get_response.json().get("sugar")
-      print(f"{dish_name} contains {calories} calories, {sodium} mgs of sodium, and {sugar} grams of sugar")
+      if post_response.status_code == 201:
+        get_response = requests.get(f"http://localhost:8000/dishes/{dish_name}")
+        calories = get_response.json().get("cal")
+        sodium = get_response.json().get("sodium")
+        sugar = get_response.json().get("sugar")
+        print(f"{dish_name} contains {calories} calories, {sodium} mgs of sodium, and {sugar} grams of sugar")
+      else:
+        print("ERROR")
 
